4dbf0f53242c9983eb998cab7cd3eef1a6c5fdcf
kashif Bacha89 <muhammadkashif3779@gmail.com>
2024-01-23 14:33:16 +0500

    updated

--- lib/view/main_view/screens/home/product_by_cat_screen.dart
+++ lib/view/main_view/screens/home/product_by_cat_screen.dart
@@ -9,6 +9,44 @@ class ProductByCatScreen extends StatefulWidget {
 class _ProductByCatScreenState extends State<ProductByCatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final height=MediaQuery.sizeOf(context).height;
+    final width=MediaQuery.sizeOf(context).width;
+    return Scaffold(
+      backgroundColor: const Color(0xFFE2E2E2),
+      body: SizedBox(
+        height: height,
+        child: Stack(
+          children: [
+            Container(
+              padding:  EdgeInsets.fromLTRB(width*.02, height*.05, 0, 0),
+              height: height*.4,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/top_image.png"),
+                      fit: BoxFit.fill)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(padding: EdgeInsets.fromLTRB(6, 12, 16, 18),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: (){},
+
+                      )
+
+                    ],
+                  ),
+                  )
+                ],
+              ),
+            ),
+
+          ],
+        ),
+
+      ),
+    );
   }
 }
