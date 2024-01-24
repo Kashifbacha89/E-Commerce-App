ef3dd1d11d52b5a5fcb2d362f92ea0fe8aa2f90b
kashif Bacha89 <muhammadkashif3779@gmail.com>
2024-01-24 17:22:35 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:e_commerce_app/controllers/main_screen_controller.dart';
+import 'package:e_commerce_app/controllers/product_controller.dart';
 import 'package:e_commerce_app/view/main_view/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => MainScreenNotifier())],
-      child: const MyApp()));
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (_) => MainScreenNotifier()),
+    ChangeNotifierProvider(create: (_)=>ProductNotifier()),
+  ], child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -19,21 +21,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
--- lib/view/main_view/screens/home/product_detail_screen.dart
+++ lib/view/main_view/screens/home/product_detail_screen.dart
@@ -1,99 +1,151 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:e_commerce_app/controllers/product_controller.dart';
+import 'package:e_commerce_app/models/sneaker_model.dart';
+import 'package:e_commerce_app/services/helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailScreen extends StatefulWidget {
-  const ProductDetailScreen({Key? key}) : super(key: key);
+  const ProductDetailScreen({Key? key, required this.id, required this.category}) : super(key: key);
+  final String id;
+  final String category;
 
   @override
   State<ProductDetailScreen> createState() => _ProductDetailScreenState();
 }
 
 class _ProductDetailScreenState extends State<ProductDetailScreen> {
-  final PageController _pageController=PageController();
+  final PageController _pageController = PageController();
+  late Future<Sneakers> _man;
+  late Future<Sneakers> _female;
+  late Future<Sneakers> _kids;
+  void getShoes(){
+    if(widget.category=="Men's Running"){
+      _man=Helper().getMaleSneakersById(widget.id);
+    }else if(widget.category=="Women's Running"){
+      _female=Helper().getFemaleSneakersById(widget.id);
+    }else if(widget.category=="Kid's Running"){
+      _kids =Helper().getKidsSneakersById(widget.id);
+
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.sizeOf(context).height;
     final width = MediaQuery.sizeOf(context).width;
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            automaticallyImplyLeading: false,
-            leadingWidth: 0,
-            pinned: true,
-            snap: false,
-            backgroundColor: Colors.transparent,
-            expandedHeight: height,
-            title: Padding(
-              padding: const EdgeInsets.only(bottom: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GestureDetector(
-                    onTap: () {},
-                    child: const Icon(
-                      AntDesign.close,
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {},
-                    child: const Icon(Ionicons.ellipsis_horizontal),
-                  )
-                ],
-              ),
-            ),
-            flexibleSpace: FlexibleSpaceBar(
-              background: Stack(
-                children: [
-                  SizedBox(
-                    height: height * 0.5,
-                    width: width,
-                    child: PageView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: 4,
-                        controller: _pageController,
-                        onPageChanged: (page){
-
-                        },
-                        itemBuilder: (context, index) {
-                          return Stack(
+        body: FutureBuilder<Sneakers>(
+          future: getShoes(),
+          builder: (context,snapshot){
+            if(snapshot.connectionState==ConnectionState.waiting){
+              return const Center(child: CircularProgressIndicator(),);
+            }else if(snapshot.hasError){
+              return Text("Error${snapshot.hasError}");
+            }else{
+              final male=snapshot.data!;
+              return Consumer<ProductNotifier>(
+                builder: (context, productNotifier, child) {
+                  return CustomScrollView(
+                    slivers: [
+                      SliverAppBar(
+                        automaticallyImplyLeading: false,
+                        leadingWidth: 0,
+                        pinned: true,
+                        snap: false,
+                        backgroundColor: Colors.transparent,
+                        expandedHeight: height,
+                        title: Padding(
+                          padding: const EdgeInsets.only(bottom: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Container(
-                                height: height*0.39,
-                                width: width,
-                                color: Colors.grey.shade300,
-                                child: CachedNetworkImage(imageUrl: '',fit: BoxFit.contain,),
+                              GestureDetector(
+                                onTap: () {},
+                                child: const Icon(
+                                  AntDesign.close,
+                                ),
                               ),
-                              Positioned(
-                                  top: height*.09,
-                                  right:20,
-                                  child: const Icon(AntDesign.heart,color: Colors.grey,)),
-                              Positioned(
-                                  top: height*0.03,
-                                  left: 0,
-                                  right:0,
-                                  bottom:0,
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-
-                                    children: List<Widget>.generate(4, (index) => const Padding(padding: EdgeInsets.symmetric(horizontal: 4),
-                                    child: CircleAvatar(
-                                      radius: 5,
-                                      backgroundColor: Colors.grey,
-                                    ),
-                                    )),))
+                              GestureDetector(
+                                onTap: () {},
+                                child: const Icon(Ionicons.ellipsis_horizontal),
+                              )
                             ],
-                          );
-
-                    }),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
+                          ),
+                        ),
+                        flexibleSpace: FlexibleSpaceBar(
+                          background: Stack(
+                            children: [
+                              SizedBox(
+                                height: height * 0.5,
+                                width: width,
+                                child: PageView.builder(
+                                    scrollDirection: Axis.horizontal,
+                                    itemCount: 4,
+                                    controller: _pageController,
+                                    onPageChanged: (page) {
+                                      productNotifier.activePage = page;
+                                    },
+                                    itemBuilder: (context, index) {
+                                      return Stack(
+                                        children: [
+                                          Container(
+                                            height: height * 0.39,
+                                            width: width,
+                                            color: Colors.grey.shade300,
+                                            child: CachedNetworkImage(
+                                              imageUrl: '',
+                                              fit: BoxFit.contain,
+                                            ),
+                                          ),
+                                          Positioned(
+                                              top: height * .09,
+                                              right: 20,
+                                              child: const Icon(
+                                                AntDesign.heart,
+                                                color: Colors.grey,
+                                              )),
+                                          Positioned(
+                                              top: height * 0.03,
+                                              left: 0,
+                                              right: 0,
+                                              bottom: 0,
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                                children: List<Widget>.generate(
+                                                    4,
+                                                        (index) => Padding(
+                                                      padding:
+                                                      const EdgeInsets.symmetric(
+                                                          horizontal: 4),
+                                                      child: CircleAvatar(
+                                                        radius: 5,
+                                                        backgroundColor:
+                                                        productNotifier
+                                                            .activePage !=
+                                                            index
+                                                            ? Colors.grey
+                                                            : Colors.black,
+                                                      ),
+                                                    )),
+                                              ))
+                                        ],
+                                      );
+                                    }),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              );
+            }
+          },
+        )
     );
   }
 }
