ec1e47843396623f1ab39838d370c3b23abfaaf5
kashif Bacha89 <muhammadkashif3779@gmail.com>
2024-01-29 16:17:29 +0500

    updated

--- lib/view/main_view/screens/favourite/favourite_screen.dart
+++ lib/view/main_view/screens/favourite/favourite_screen.dart
@@ -1,5 +1,7 @@
 import 'package:e_commerce_app/utils/app_utils/app_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+
 class FavouriteScreen extends StatefulWidget {
   const FavouriteScreen({Key? key}) : super(key: key);
 
@@ -8,12 +10,44 @@ class FavouriteScreen extends StatefulWidget {
 }
 
 class _FavouriteScreenState extends State<FavouriteScreen> {
+  final _favBox = Hive.box('fav_box');
+  _deleteFav(int key) async {
+    await _favBox.delete(key);
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<dynamic> fav=[];
+    final favData=_favBox.keys.map((key){
+      final item=_favBox.get(key);
+      return {
+        "key":key,
+        "id":item["id"],
+        "category":item["category"],
+        "price":item['price'],
+        "imageUrl":item['imageUrl']
+      };
+
+    }).toList();
+    fav=favData.reversed.toList();
+    final height = MediaQuery.sizeOf(context).height;
+    final width = MediaQuery.sizeOf(context).width;
     return Scaffold(
-      body: Center(
-        child: Text('My Favourite',style: appStyle(36, Colors.black, FontWeight.w500),),
+        body: SizedBox(
+      height: height,
+      width: width,
+      child: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(width * .02, height * .05, 0, 0),
+            height: height * .4,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/top_image.png"),
+                    fit: BoxFit.fill)),
+          )
+        ],
       ),
-    );
+    ));
   }
 }
